225dc800748ad3be3d11879df75826a90fe19601
Andrew Rosario <rosario.andrew@gmail.com>
2019-11-26 18:17:17 -0600

    initial commit

---
+++ welcome/WelcomeContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class WelcomeContainer extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <h1>Welcome!!</h1>
+            </div>
+         );
+    }
+}
+ 
+export default WelcomeContainer;
